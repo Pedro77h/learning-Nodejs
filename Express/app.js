26d6babe3d430583b9cf17340ffb851b632e5e2b
Pedro <pedro@pop-os.localdomain>
2022-06-11 15:23:49 -0300

    Aprendendo Nodejs - Primeiro Projeto usando o express

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const { request } = require("http")
 const {randomUUID} = require("crypto")
+const { response } = require("express")
+const fs = require("fs")
 
 const app = express()
-const products = []
+let products = []
+
+fs.readFile("products.json" , "utf-8" , (err , data)=>{
+    if(err){
+        console.log(err)
+    }else{
+        products = JSON.parse(data)
+    }
+})
 
 app.use(express.json())
 
@@ -18,11 +28,63 @@ app.post("/produtos" , (request , response) =>{
 
     products.push(product)
 
+        
+
+        productfile()
         return response.json(product)
+})
+
     app.get("/produtos" , (request , response) =>{
+        
         return response.json(products)
+})
+
+app.get("/produtos/:id" , (request , response)=>{
+    const {id} = request.params
+    const product = products.find(product => product.id === id)
+    return response.json(product)
+})
+
+app.put("/produtos/:id" , (request , response) =>{
+
+    const {id} = request.params
+    const { name , price} = request.body
+
+    const productindex = products.findIndex(product => product.id === id)
+    products[productindex] = {
+        ...products[productindex] ,
+        name , 
+        price,
+    }
+    productfile()
+    return response.json({
+        message: "Produto alterado com sucesso" 
+    })
+
+})
+
+app.delete("/produtos/:id" , (request , response) =>{
+    const {id} = request.params
+    const productindex = products.findIndex(product => product.id === id)
+
+    products.splice(productindex , 1)
+
+    return response.json({
+        message: "Produto removido com sucesso"
     })
 })
 
+function productfile(){
+    fs.writeFile( "products.json", JSON.stringify(products), (err) =>{
+        if (err){
+            console.log(err)
+        }else{
+            console.log("produto inserido com sucesso")
+        }
+    })
+
+}
+
 app.listen(4030 , ()=> console.log("Servidor esta rodando na porta 4030"))
+
 
