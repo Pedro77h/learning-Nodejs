84a9af8e8806f45d922a4adbb9380d1e97775954
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-08-02 21:50:49 -0300

    Aprendendo Nodejs - Token modification

--- API NodeJS + Express + Mongo/src/middleware/auth.js
+++ API NodeJS + Express + Mongo/src/middleware/auth.js
@@ -3,23 +3,25 @@ const authConfig = require("../config/auth.json")
 
 
 module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization
 
     if (!authHeader) {
         return res.status(401).send({ error: "No token provied" })
     }
 
-    const parts = authHeader.split(' ');
+    const parts = authHeader.split(' ')
 
-    if (!parts === 2) {
+    if (parts.length === 2) {
+        console.log("aaaaaaaaa")
         return res.status(401).send({ error: "Token error" })
+        
     }
 
-    const { schema, token } = parts;
+    const [ scheme, token ] = parts;
 
-    if (!/^Bearer$/i.test(schema)){
+  /*   if (!/^Bearer$/i.test(scheme)){
         return res.status(401).send({ error:"token malformated"})
-    }
+    } */
 
     jwt.verify(token , authConfig.secret , (err, decoded) =>{
         if(err) return res.status(401).send ({error: "token dont match"})
