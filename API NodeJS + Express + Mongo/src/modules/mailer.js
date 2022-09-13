81cedf83ded0464837db5786350dc86b72c6cf06
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-09-13 19:15:02 -0300

    Aprendendo NodeJS - Função enviar email

--- API NodeJS + Express + Mongo/src/modules/mailer.js
+++ API NodeJS + Express + Mongo/src/modules/mailer.js
@@ -1,20 +1,25 @@
 const nodemailer = require('nodemailer')
 const hbs = require('nodemailer-express-handlebars')
 const path = require('path')
-const { host , port , user , pass} = require('../config/mail.json')
+const { host, port, user, pass } = require('../config/mail.json')
 
 var transport = nodemailer.createTransport({
-    host,
-    port,
-    auth: { user , pass
-    }
-  });
+  host,
+  port,
+  auth: {
+    user, pass
+  }
+});
 
 
-  transport.use('compile' , hbs({
-    viewEngine: 'handlebars' ,
-    viewPath: path.resolve('./src/resouces/mail/') , 
-    extName: '.html'
-  }))
+transport.use('compile', hbs({
+  viewEngine: {
+    extname: '.html',
+    layoutsDir: './src/resources/mail/auth',
+    defaultLayout: 'forgot_password',
+  },
+  viewPath: path.resolve('./src/resources/mail/'),
+  extName: '.html'
+}))
 
-  module.exports = transport
+module.exports = transport
