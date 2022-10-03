c9859f88536c80ad26b1dd8c92fafbc5dd667d80
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-10-02 21:13:28 -0300

    Aprendendo NodeJS - Node com TypeScript

--- Node-Typescript/src/index.ts
+++ Node-Typescript/src/index.ts
@@ -1,16 +1,8 @@
 /* eslint-disable @typescript-eslint/class-name-casing */
-import { Controller, Get } from '@overnightjs/core';
-import {Request , Response} from 'express'
-import {app} from './server/server'
 
-@Controller('/')
-export class rota {
-    
-    @Get(':id')
-    get(req:Request , res: Response){
-        console.log(req.params.id)
-        return res.status(200).send({msg: 'get concluido'})
-    }
+import { Express , Request , Response, Router} from 'express'
+import {serverInit} from './server/server'
 
 
-}
+new serverInit(4030).getApp()
+
--- Node-Typescript/src/server/server.ts
+++ Node-Typescript/src/server/server.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/class-name-casing */
-
+import bodyparser from 'body-parser'
 import { Server } from '@overnightjs/core'
+import mongoose from 'mongoose'
 
 export class serverInit extends Server {
     constructor(private port = 4030){
         super()
+        this.setupExpress()
+        this.routes()
+        this.database()
     }
 
     public getApp():void{
@@ -14,6 +18,20 @@ export class serverInit extends Server {
         } )
         
     }
+
+    private setupExpress():void {
+        this.app.use(bodyparser.json())
+    }
+
+    private database():void {
+        mongoose.connect('mongodb://localhost/typescript')
+    }
+
+    private routes():void {
+        this.app.get('/' , (req , res) =>{
+            return res.status(200)
+        })
+    }
+
 }
 
-export const app = new serverInit
