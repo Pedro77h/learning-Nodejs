ee64b5c02fa6fe5969d81bfa4d109a3af8f80942
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2022-12-22 23:48:16 -0300

    learning NodeJS - Solid principles

---
+++ SOLID/src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+
+  constructor(
+    private createUserUseCase: CreateUserUseCase,
+  ){}
+
+  async handle(req: Request, res: Response): Promise<Response>{
+
+    try {
+   
+      const { name, email, password } = req.body
+    
+      await this.createUserUseCase.execute({
+        name,
+        email,
+        password
+      })
+
+      return res.status(201).send()
+    }
+    catch (err) {
+      res.status(400).json({
+        message: err.message || 'Unexpected error'
+      })
+    }
+  };
+}
